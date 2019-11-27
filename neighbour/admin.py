deacbf3a1ce6d28b54f84e30403742c50140135c
jecinta <jecintawanjirug@gmail.com>
2019-11-27 15:26:07 +0300

    this registers the three classes in models.py in the admin.py

--- neighbour/admin.py
+++ neighbour/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from .models import Neighborhood,Occupants,Bussiness
 # Register your models here.
+
+admin.site.register(Neighborhood)
+admin.site.register(Occupants)
+admin.site.register(Bussiness)
