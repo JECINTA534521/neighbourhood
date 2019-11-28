492a174854e149f7772e61885f1cb6ea49d5add1
jecinta <jecintawanjirug@gmail.com>
2019-11-28 12:01:11 +0300

    this adds a function where a user can add a bussiness

--- neighbour/views.py
+++ neighbour/views.py
@@ -14,8 +14,8 @@ def signup(request):
     return render(request,'registration/registration_form.html',{'form':form})
 
 def profile(request):
-    #user = request.user
-    user_object = get_object_or_404( pk=user)
+    user = request.user
+    #user_object = get_object_or_404( request, pk=user)
     businesses= Bussiness.objects.filter(owner=user)
     prof = Occupants.objects.get(name__id=user.id)
     return render(request,'profile.html',{'user':user,'prof':prof,'businesses':businesses})
@@ -60,7 +60,7 @@ def neighborhood_details(request):
     user = Occupants.objects.get(name=request.user.id)
     neighborhood= Neighborhood.objects.get(name=user.neighborhood)
     details = Occupants.objects.filter(neighborhood__name=neighborhood)
-    # occupants=neighborhood.all
+    #occupants=neighborhood.all
     return render(request,'details.html',{'details':details})
 
 def business(request):
@@ -73,12 +73,12 @@ def add_business(request):
     user = Occupants.objects.get(name=owner)
     if request.method =='POST':
         neighborhood= Neighborhood.objects.get(name=user.neighborhood)
-        owner= Business(owner=owner,neighborhood=neighborhood)
-        form = BusinessForm(request.POST,instance=owner)
+        owner= Bussiness(owner=owner,neighborhood=neighborhood)
+        form = BussinessForm(request.POST,instance=owner)
         if form.is_valid():
             form.save()
             return redirect('businesses')
 
     else:
-        form = BusinessForm()
+        form = BussinessForm()
     return render (request,'add_business.html',{'form':form})
